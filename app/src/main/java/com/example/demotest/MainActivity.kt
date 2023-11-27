a84979ef854c0492bf332749a592be055eb8bfe9
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-11-27 15:28:59 +0500

    screen layout fragment_authorization.xml

--- app/src/main/java/com/example/demotest/MainActivity.kt
+++ app/src/main/java/com/example/demotest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.demotest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.demotest.databinding.ActivityMainBinding
+import com.example.demotest.presentation.AuthorizationFragment
+import com.example.demotest.utilits.APP_ACTIVITY
+import com.example.demotest.utilits.replaceFragmentMain
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        APP_ACTIVITY = this
+        replaceFragmentMain(AuthorizationFragment())
     }
 }
---
+++ app/src/main/java/com/example/demotest/utilits/Const.kt
@@ -0,0 +1,5 @@
+package com.example.demotest.utilits
+
+import com.example.demotest.MainActivity
+
+lateinit var APP_ACTIVITY : MainActivity
